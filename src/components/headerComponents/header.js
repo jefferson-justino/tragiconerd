4c6dd86e33de99568630201b98497d2436fb88e3
jefferson-justino <115052745+jefferson-justino@users.noreply.github.com>
2023-12-22 17:15:35 -0300

    3

---
+++ src/components/headerComponents/header.js
@@ -0,0 +1,28 @@
+import imageLogo from "../../img/logo transparente.png"
+import styles from './header.module.css'
+import azul from '../mainComponents/main'
+function Header() {
+    return (
+        <header >
+            <section>
+                <img src={imageLogo} alt="LogoImagem" className={styles.image} />
+                <h1 className={styles.title}>TRAGICO NERD</h1>
+            </section>
+
+            <nav>
+                <ul className={styles.list}>
+                    <a href= {azul} target="_blank" rel="noreferrer" > <li className={styles.option}>SOBRE</li></a>
+                    <a href="#jj" target="_blank" rel="noreferrer" > <li className={styles.option}>SETUP</li></a>
+                    <a href="#jj" target="_blank" rel="noreferrer" > <li className={styles.option}>DOAR</li></a>
+                </ul>
+            </nav>
+
+            <section>
+                <a href="https://www.amazon.com.br/hz/wishlist/ls/K7KBOEU4EEX0?ref_=wl_share" target="_blank" rel="noreferrer" ><h1 className={styles.wishList}>LISTA DE DESEJOS</h1></a>
+            </section>
+        </header>
+
+    );
+}
+
+export default Header;
---
+++ src/components/mainComponents/main.js
@@ -0,0 +1,57 @@
+import imageLogo from "../../img/logo transparente.png"
+import styles from'./main.module.css'
+
+export default function Main(){
+    let azul = 'about'
+    return(
+        <main>
+
+            <h2 className={styles.about} id={azul} >Me Conheça</h2>
+           <section className={styles.aboutChanel}>
+            <h3 className={styles.description}>
+                Olá, você está visitando o site do nosso canal, mas já deve ter se perguntado, quem sou eu? 
+                Bom, você pode me chamar de Jeff, tenho 21 anos e sou eu quem cuido de todos os vídeos de todas as redes 
+                oficiais  do canal. Sou apaixonado pelo mundo gamer, adoro jogar e me divertir neste universo, 
+                principalmente com amigos. Jogo no Pc e via nuvem, não tenho um Pc super potente (ainda) para rodar 
+                tudo em alta resolução, mas faço o que posso. Mas é isso, sou um jovem que assim como você adora jogar 
+                e se divertir, então, venha comigo entrar nesse mundo maravilhoso. Eu tenho certeza que você não vai sair
+                 de nenhum video sem dar pelo menos uma risada hahah. Essa é a minha vibe, me divertir 
+                 e passar isso para vocês.
+            </h3>
+           
+           
+           <figure>
+           <img src={imageLogo} alt="Logo"/>
+            
+            </figure>
+            </section>
+            
+            <article> 
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/udDgL1zy9eI?si=MiZQ2qTF8xbu-qwV" 
+                title="YouTube video player" frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                 allowfullscreen>
+            </iframe>
+            </article>
+
+                <section className={styles.setup}>
+                    <h2 className={styles.setupTitle} id="about">Meu Setup</h2>
+                       
+                    <ul>
+                        <a href="https://amzn.to/3tjjRCd" target="_blank" rel="noreferrer" className={styles.product}><li>Processador: I5 6600;</li></a>
+                        <a href="https://amzn.to/3NCjwkI" target="_blank" rel="noreferrer" className={styles.product}><li>Placa mãe: Gigabyte H110M-S2V-CF</li></a>
+                        <a href="https://amzn.to/3vig5cz" target="_blank" rel="noreferrer"className={styles.product}><li>Placa de vídeo: GTX 750;</li></a>
+                        <a href="https://amzn.to/3GTzhjC" target="_blank" rel="noreferrer"className={styles.product}><li>Memória RAM:  12GB;</li></a>
+                        <a href="https://amzn.to/48q03Mf" target="_blank" rel="noreferrer"className={styles.product}><li>Armazenamento: SSD 120GB & HDD 500GB;</li></a>
+                        <a href="https://amzn.to/3Rw21E4" target="_blank" rel="noreferrer"className={styles.product}><li>Fonte: 500W;</li></a>
+                        <a href="https://amzn.to/3RPFQKj" target="_blank" rel="noreferrer"className={styles.product}><li>Headset: HyperX Cloud Stinger S;</li></a>
+                        <a href="https://amzn.to/3tCiiPA" target="_blank" rel="noreferrer"className={styles.product}><li>Teclado: Teclado semi mecânico</li></a>
+                        <a href="https://amzn.to/3RRgYSp" target="_blank" rel="noreferrer"className={styles.product}><li>Mouse: M7 G Pro</li></a>
+                    </ul>
+
+                </section>
+
+        </main>
+
+    )
+}
