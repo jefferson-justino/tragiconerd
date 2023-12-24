7b9d7e96c1aee1bc63f2a286e1f6d37965193305
jefferson-justino <115052745+jefferson-justino@users.noreply.github.com>
2023-12-24 11:21:41 -0300

    add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './app.css'
 import Main from './components/mainComponents/main';
 import Header from './components/headerComponents/header';
+import Footer from './components/footerComponents/footer';
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
-
+      <Footer/>
 
     </div>
   );
