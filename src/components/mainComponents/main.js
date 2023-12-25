ef36ed4dc0172ad0d15eeba4713ccf12bd9681cf
jefferson-justino <115052745+jefferson-justino@users.noreply.github.com>
2023-12-25 10:18:01 -0300

    ,

--- src/components/mainComponents/main.js
+++ src/components/mainComponents/main.js
@@ -30,11 +30,12 @@ export default function Main(){
             </section>
             
             <article> 
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/udDgL1zy9eI?si=MiZQ2qTF8xbu-qwV" 
-                title="YouTube video player" frameborder="0" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                 allowfullscreen>
-            </iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/sPtWCqajvfc?si=aqMeY5amoy2mGEOw"
+             title="YouTube video player" frameborder="0" 
+             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+             allowfullscreen>
+                
+             </iframe>
 
             </article>
              <div className={styles.groupSection}>
